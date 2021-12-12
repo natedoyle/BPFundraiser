6ee5f278289f625d4bfdcf90fff50cc2035e55dd
Nate Doyle <nate@automox.com>
2021-12-12 15:31:08 -0700

    added another method for creating payments.

--- README.txt
+++ README.txt
@@ -3,3 +3,4 @@ https://support.stripe.com/questions/how-to-accept-donations-through-stripe
 TODO - need to finish integration to cycle user back to success page.
 TODO - need to test terminal integration with stripe page. https://stripe.com/docs/terminal/payments/setup-sdk
 TODO - need to turn on and test email confirmation
+TODO - checkout api with perhaps variable cost - https://stripe.com/docs/payments/quickstart
--- server.py
+++ server.py
@@ -4,27 +4,37 @@
 import os
 import stripe
 
-from flask import Flask, redirect, send_from_directory, render_template
+from flask import Flask, redirect, send_from_directory, render_template, jsonify
 
 app = Flask(__name__)
 
 stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+connection_token = stripe.terminal.ConnectionToken.create()
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def send_root():
-    return send_from_directory('', 'index.html')
+  return send_from_directory('', 'index.html')
+
+@app.route('/index.html', methods=['GET'])
+def send_index():
+  return send_from_directory('', 'index.html')
 
 @app.route('/success.html', methods=['GET'])
 def send_success():
   return send_from_directory('pages', 'success.html')
 
-@app.route('/cancel.html')
+@app.route('/cancel.html', methods=['GET'])
 def send_cancel():
-  return send_from_directory('', 'cancel.html')
+  return send_from_directory('pages', 'cancel.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
-  return send_from_directory('', '404.html')
+  return send_from_directory('pages', '404.html')
+
+@app.route('/connection_token', methods=['POST'])
+def token():
+  token = connection_token
+  return jsonify(secret=token.secret)
 
 @app.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
@@ -33,7 +43,7 @@ def create_checkout_session():
       'price_data': {
         'currency': 'usd',
         'product_data': {
-          'name': 'T-shirt',
+          'name': 'Donation',
         },
         'unit_amount': 2000,
       },
@@ -45,6 +55,24 @@ def create_checkout_session():
   )
 
   return redirect(session.url, code=303)
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        # Create a PaymentIntent with the order amount and currency
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='usd',
+            automatic_payment_methods={
+                'enabled': True,
+            },
+        )
+        return jsonify({
+            'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403
 
 if __name__== '__main__':
     app.run(port=4242)
