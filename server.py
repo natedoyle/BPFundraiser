805a7aa3b86fe54364d8f27393479e3efa15d122
Nate Doyle <nate@automox.com>
2021-12-11 17:21:49 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+# This example sets up an endpoint using the Flask framework.
+# Watch this video to get started: https://youtu.be/7Ul1vfmsDck.
+
+import os
+import stripe
+
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+  session = stripe.checkout.Session.create(
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'T-shirt',
+        },
+        'unit_amount': 2000,
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url='https://example.com/success',
+    cancel_url='https://example.com/cancel',
+  )
+
+  return redirect(session.url, code=303)
+
+if __name__== '__main__':
+    app.run(port=4242)
